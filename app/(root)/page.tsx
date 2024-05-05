4468a2217ef3316a33afd555bb1fd90d7f1f96e6
mohamed elamrani <mohammadamrani475@gmail.com>
2024-05-05 10:03:21 -0400

    right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-  const loggedIn={firstName:"mohamed"};
+  const loggedIn={firstName:"mohamed",lastName:"chahid",email:"mohammad123@gmail.com"};
   return (
     <section className='home'>
       <div className='home-content'>
@@ -20,9 +21,15 @@ const Home = () => {
             totalBanks={1}
             totalCurrentBalance={1250.78}
           />
-
         </header>
+
+        RecentTransactions
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:125.2},{currentBalance:147.55}]}
+      />
 
     </section>
   )
